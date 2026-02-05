fdae1c8e763ecb2ad5d2d3022ed43c6d95fb1caf
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-02-05 12:37:29 +0000

    feat: Mapping with Result (#3)
    * add mapper with result
    
    * abstract and separate per class
    
    * make internal to enable unit testing
    
    * add tests for mapping result, upgrade packages
    
    * remove ref to app

---
+++ src/DfE.CleanArchitecture.CrossCutting/Mapper/IMapperWithResult.cs
@@ -0,0 +1,14 @@
+﻿namespace DfE.CleanArchitecture.Common.CrossCutting.Mapper;
+public interface IMapperWithResult<in TMapFrom, TMapTo>
+{
+    /// <summary>
+    /// Defines the method call for mapping one type to another.
+    /// </summary>
+    /// <param name="input">
+    /// The type to map from.
+    /// </param>
+    /// <returns>
+    /// The type to map to.
+    /// </returns>
+    IMappedResult<TMapTo> Map(TMapFrom input);
+}
---
+++ src/DfE.CleanArchitecture.CrossCutting/Mapper/MappedResult.cs
@@ -0,0 +1,42 @@
+﻿namespace DfE.CleanArchitecture.Common.CrossCutting.Mapper;
+public record MappedResult<TResult> : IMappedResult<TResult>
+{
+    private readonly string _errorMessage;
+
+    internal MappedResult(MappingResultStatus status, TResult? result, Exception? ex = null)
+    {
+        if (status is MappingResultStatus.Successful && ex is not null)
+        {
+            throw new ArgumentException("Cannot have a successful mapping result with a captured exception", ex);
+        }
+
+        Result = result;
+        Status = status;
+        Exception = ex;
+        _errorMessage = ex?.Message ?? string.Empty;
+    }
+
+    internal MappedResult(MappingResultStatus status, TResult? result, string? errorMessage)
+    {
+        if (status is MappingResultStatus.Successful && !string.IsNullOrWhiteSpace(errorMessage))
+        {
+            throw new ArgumentException("Cannot create a successful mapping result with an error message");
+        }
+
+        Result = result;
+        Status = status;
+        _errorMessage = errorMessage ?? string.Empty;
+    }
+
+    public MappingResultStatus Status { get; init; }
+    public TResult? Result { get; }
+    public Exception? Exception { get; }
+    public bool HasResult => Result is not null && Status == MappingResultStatus.Successful;
+    public string ErrorMessage => HasResult ? string.Empty : _errorMessage;
+
+    public static MappedResult<TResult> Success(TResult result) => new(MappingResultStatus.Successful, result);
+
+    public static MappedResult<TResult> RequestError(string errorMessage) => new(MappingResultStatus.MapFromArgumentError, default, errorMessage);
+
+    public static MappedResult<TResult> MappingError(Exception ex) => new(MappingResultStatus.MapToError, default(TResult), ex);
+}
